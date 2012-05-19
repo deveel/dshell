2f951a2b771c53939a162549e3440959bec6f441
Antonello Provenzano <antonello@deveel.com>
2012-05-19 02:36:51 +0200

    Added NUnit tests for automatic tests on the application library

---
+++ src/dshell-nunit.vs2010/Deveel.Console/StringWriteEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Deveel.Console {
+	public delegate void StringWriteEventHandler(object sender, StringWriteEventArgs args);
+
+	public sealed class StringWriteEventArgs : EventArgs {
+		public readonly string s;
+
+		public StringWriteEventArgs(string s) {
+			this.s = s;
+		}
+
+		public string Value {
+			get { return s; }
+		}
+	}
+}
---
+++ src/dshell-nunit.vs2010/Deveel.Console/TestOutputDevice.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Deveel.Console {
+	class TestOutputDevice : OutputDevice {
+		private readonly TextWriter outputSink;
+		private readonly List<string> stringList;
+
+		public TestOutputDevice() {
+			outputSink = new OutputSink(this);
+			stringList = new List<string>();
+		}
+
+		public event StringWriteEventHandler StringWritten;
+
+		public override Encoding Encoding {
+			get { return Encoding.ASCII; }
+		}
+
+		protected override TextWriter Output {
+			get { return outputSink; }
+		}
+
+		public IEnumerable<string> Strings {
+			get { return stringList.AsReadOnly(); }
+		}
+
+		protected void OnStringWritten(string s) {
+			if (StringWritten != null)
+				StringWritten(this, new StringWriteEventArgs(s));
+
+			stringList.Add(s);
+		}
+
+		#region OutputSink
+
+		class OutputSink : TextWriter {
+			private readonly TestOutputDevice outputDevice;
+
+			public OutputSink(TestOutputDevice outputDevice) {
+				this.outputDevice = outputDevice;
+			}
+
+			public override Encoding Encoding {
+				get { return outputDevice.Encoding; }
+			}
+
+			public override void Write(char[] buffer, int index, int count) {
+				outputDevice.OnStringWritten(new string(buffer, index, count));
+			}
+		}
+
+		#endregion
+	}
+}
